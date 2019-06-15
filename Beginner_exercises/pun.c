b351826735a76165a131926ca030aa427953bd5f
lnfinem <camilohoyos0499@gmail.com>
2019-06-15 00:38:37 -0500

    Adicionando dos archivos de principiante

---
+++ Beginner_exercises/pun.c
@@ -0,0 +1,14 @@
+/*
+* Name: pun.c
+* Purpose: Prints a bad pun.
+* Author: Juan Camilo Hoyos
+*/
+
+#include <stdio.h>
+
+int main(void)	// Beggining of main program
+{
+	printf("To C, or not to C: that is the question.\n");
+	printf("that is the question.\n");
+	return 0;
+}
